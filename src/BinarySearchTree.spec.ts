6af5878b22f7bee0622cd5b3d4932c0011c9be19
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-06-08 16:45:22 -0400

    started binary search tree implementation

--- src/BinarySearchTree.spec.ts
+++ src/BinarySearchTree.spec.ts
@@ -1,7 +1,14 @@
 import * as test from "tape";
 
-import (BinarySearchTree) from "./BinarySearchTree";
+import {BinarySearchTree} from "./BinarySearchTree";
 
 test('BinarySearchTree', (t: test.Test): void => {
+    t.test('getNodeData', (t: test.Test): void => {
+        const testTree = new BinarySearchTree<number>((a, b) => {
+            return a - b;
+        }, 1);
 
+        t.equal(testTree.getNodeData(), 1);
+        t.end();
+    });
 });
--- src/BinarySearchTree.ts
+++ src/BinarySearchTree.ts
@@ -1,11 +1,16 @@
 import {ITree} from "./Tree";
 
-export class BinarySearchTree<T> implements ITree<T> {
+export class BinarySearchTree<T> {
     private _sortFunction: Function;
     private _data: T;
+    private _tree: BinarySearchTree<T>;
 
-    constructor(sortFunction: Function, data?: T) {
+    constructor(sortFunction: (a: T, b: T) => number, data?: T) {
         this._sortFunction = sortFunction;
         this._data = data;
+    }
+
+    public getNodeData(): T {
+        return this._data;
     }
 }
