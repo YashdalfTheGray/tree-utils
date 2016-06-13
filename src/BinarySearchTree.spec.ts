9952cb77be9827cf8e8480d3f64440819998528f
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-06-12 21:56:18 -0400

    fixed tests, added one for checking constructor

--- src/BinarySearchTree.spec.ts
+++ src/BinarySearchTree.spec.ts
@@ -3,23 +3,28 @@ import * as test from "tape";
 import {BinarySearchTree} from "./BinarySearchTree";
 
 test('BinarySearchTree', (t: test.Test): void => {
+
+    t.test('creates a new Tree instance with the passed in data', (t: test.Test) => {
+        const treeInstance = new BinarySearchTree<string>((a, b) => a.length - b.length, 'test');
+
+        t.assert(treeInstance !== undefined, 'instance exists');
+        t.equal(treeInstance.getNodeData(), 'test');
+        t.end();
+    });
+
     t.test('getNodeData', (t: test.Test): void => {
-        const testTree = new BinarySearchTree<number>((a, b) => {
-            return a - b;
-        }, 1);
+        const testTree = new BinarySearchTree<number>((a, b) => a - b, 1);
 
         t.equal(testTree.getNodeData(), 1);
         t.end();
     });
 
     t.test('setNodeData', (t: test.Test): void => {
-        const testTree = new BinarySearchTree<number>((a, b) => {
-            return a - b;
-        });
+        const testTree = new BinarySearchTree<number>((a, b) => a - b);
 
         t.equal(testTree.getNodeData(), undefined);
         testTree.setNodeData(2);
-        t.equal(testTree.getNodeData, 2);
+        t.equal(testTree.getNodeData(), 2);
         t.end();
     });
 });
