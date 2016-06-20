d3a382aed4976661589b8a5b43369ff500149eda
YashdalfTheGray <yash.kulshrestha@gmail.com>
2016-06-20 17:52:10 -0400

    added jsdoc to the built versions

--- dist/BinaryTree.d.ts
+++ dist/BinaryTree.d.ts
@@ -7,14 +7,56 @@ export declare class BinaryTree<T> implements ITree<T> {
     private _data;
     private _leftChild;
     private _rightChild;
+    /**
+     * Creates a new BinaryTree instance
+     * @param  {T}      data the data to store at the root node.
+     */
     constructor(data?: T);
+    /**
+     * Returns the data stored at the node
+     * @return {T} the data
+     */
     getNodeData(): T;
+    /**
+     * Set the data at the node
+     * @param {T} data the data to store at the node
+     */
     setNodeData(data: T): void;
+    /**
+     * Add a child tree to the node
+     * @param {T} node the data to store at the new child location
+     */
     addChild(node: T, index?: ChildLocation): void;
+    /**
+     * Returns the child at the given location
+     * @param  {number}        index the index of the child to get
+     * @return {BinaryTree<T>}       the child at the requested position
+     */
     getChildAt(loc: ChildLocation): BinaryTree<T>;
+    /**
+     * Removes the child at a given location and returns it
+     * @param  {number}        index the index of the child to remove
+     * @return {BinaryTree<T>}       the removed child tree
+     */
     removeChild(loc: ChildLocation): BinaryTree<T>;
+    /**
+     * Returns the size, i.e. the number of nodes in the tree
+     * @return {number} the size of the tree
+     */
     size(): number;
+    /**
+     * Returns how many levels of nodes the tree has
+     * @return {number} the height of the tree
+     */
     height(): number;
+    /**
+     * Returns the number of direct children the tree has
+     * @return {number} the number of direct children
+     */
     numChildren(): number;
+    /**
+     * Creates a new tree with the same data
+     * @return {BinaryTree<T>} a new tree that is a clone of this one
+     */
     clone(): BinaryTree<T>;
 }
--- dist/BinaryTree.js
+++ dist/BinaryTree.js
@@ -6,15 +6,31 @@ var lodash_1 = require("lodash");
 })(exports.ChildLocation || (exports.ChildLocation = {}));
 var ChildLocation = exports.ChildLocation;
 var BinaryTree = (function () {
+    /**
+     * Creates a new BinaryTree instance
+     * @param  {T}      data the data to store at the root node.
+     */
     function BinaryTree(data) {
         this._data = data;
     }
+    /**
+     * Returns the data stored at the node
+     * @return {T} the data
+     */
     BinaryTree.prototype.getNodeData = function () {
         return this._data;
     };
+    /**
+     * Set the data at the node
+     * @param {T} data the data to store at the node
+     */
     BinaryTree.prototype.setNodeData = function (data) {
         this._data = data;
     };
+    /**
+     * Add a child tree to the node
+     * @param {T} node the data to store at the new child location
+     */
     BinaryTree.prototype.addChild = function (node, index) {
         index = index || ChildLocation.LEFT;
         if (index === ChildLocation.LEFT) {
@@ -24,9 +40,19 @@ var BinaryTree = (function () {
             this._rightChild = new BinaryTree(node);
         }
     };
+    /**
+     * Returns the child at the given location
+     * @param  {number}        index the index of the child to get
+     * @return {BinaryTree<T>}       the child at the requested position
+     */
     BinaryTree.prototype.getChildAt = function (loc) {
         return loc === ChildLocation.LEFT ? this._leftChild : this._rightChild;
     };
+    /**
+     * Removes the child at a given location and returns it
+     * @param  {number}        index the index of the child to remove
+     * @return {BinaryTree<T>}       the removed child tree
+     */
     BinaryTree.prototype.removeChild = function (loc) {
         var removedChild = this.getChildAt(loc).clone();
         if (loc === ChildLocation.LEFT) {
@@ -37,6 +63,10 @@ var BinaryTree = (function () {
         }
         return removedChild;
     };
+    /**
+     * Returns the size, i.e. the number of nodes in the tree
+     * @return {number} the size of the tree
+     */
     BinaryTree.prototype.size = function () {
         if (!this._leftChild && !this._rightChild) {
             return 1;
@@ -51,6 +81,10 @@ var BinaryTree = (function () {
             return 1 + this._leftChild.size() + this._rightChild.size();
         }
     };
+    /**
+     * Returns how many levels of nodes the tree has
+     * @return {number} the height of the tree
+     */
     BinaryTree.prototype.height = function () {
         if (!this._leftChild && !this._rightChild) {
             return 1;
@@ -65,6 +99,10 @@ var BinaryTree = (function () {
             return 1 + lodash_1.max([this._leftChild.height(), this._rightChild.height()]);
         }
     };
+    /**
+     * Returns the number of direct children the tree has
+     * @return {number} the number of direct children
+     */
     BinaryTree.prototype.numChildren = function () {
         if (this._leftChild && this._rightChild) {
             return 2;
@@ -76,6 +114,10 @@ var BinaryTree = (function () {
             return 0;
         }
     };
+    /**
+     * Creates a new tree with the same data
+     * @return {BinaryTree<T>} a new tree that is a clone of this one
+     */
     BinaryTree.prototype.clone = function () {
         var newTree = new BinaryTree();
         newTree._data = this._data;
